8323a5240761090c1d90baeb13a8921ead16a7fb
Alex Mit <klepoz.672@mail.ru>
2023-12-21 06:27:49 +0300

    change pagination justification

--- src/components/common/Pagination/index.styled.ts
+++ src/components/common/Pagination/index.styled.ts
@@ -17,7 +17,7 @@ export const Content = styled.div`
   width: 100%;
   height: 100%;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   box-sizing: border-box;
 `;
 
