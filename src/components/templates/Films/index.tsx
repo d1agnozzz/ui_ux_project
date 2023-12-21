b83e3adf5a99a1e0a76774739c85ebe563bb1bd3
Alex Mit <klepoz.672@mail.ru>
2023-12-21 06:26:04 +0300

    add page in url

--- src/components/templates/Films/index.tsx
+++ src/components/templates/Films/index.tsx
@@ -1,11 +1,22 @@
 import { useFilmList } from "@/lib/hooks/useFilmList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Film from "./Film";
+import { useRouter } from "next/router";
 import * as Styled from "./index.styled";
 import Pagination from "@/components/common/Pagination";
+import { usePathname } from "next/navigation";
 
 const Films = () => {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState("1");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) {
+      console.log(router.query);
+      setPage(router.query.current_page || "");
+    }
+  }, [router.isReady]);
+
   const pageSize = 24;
   const { filmList, isLoading } = useFilmList(String(page), String(pageSize));
 
@@ -23,7 +34,10 @@ const Films = () => {
         pageSize={pageSize}
         currentPage={filmList?.data.page_number}
         totalMovieCount={filmList?.data.movie_count}
-        onPageChange={(page) => setPage(page)}
+        onPageChange={(page) => {
+          setPage(page);
+          router.push(`/catalog/${page}`);
+        }}
       />
     </Styled.Container>
   ) : null;
